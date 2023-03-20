ce6a0d312de284e9171decd4ddd0c2413b87350d
Josh06 <Admin@chaucongtu.gq>
2023-03-20 17:42:37 +0700

    feat: add feature post, cart

---
+++ app/views/cart/cart.php
@@ -0,0 +1,109 @@
+<div class="container">
+    <div class="cart">
+        <div class="cart-left panel">
+            <div class="cart-header">Giỏ hàng của bạn</div>
+            <div class="cart-count">
+                Bạn đang có <strong>3</strong> sản phẩm trong giỏ hàng
+            </div>
+            <div class="cart-body">
+                <div class="cart-product">
+                    <div class="cart-product-img">
+                        <img src="/img/oppo-reno8-pro.jpg">
+                    </div>
+                    <div class="cart-product-name">
+                        Điện thoại Oppo Reno 8 Pro
+                        <br>
+                        <a href="" class="link-detail">Xem chi tiết</a>
+                    </div>
+                    <div class="cart-product-price">
+                        21.000.000đ
+                        <div class="change-count-product">
+                            <span class="change-count">- </span><span id="count-value"> 1 </span><span class="change-count"> +</span>
+                        </div>
+                    </div>
+                    <a href="" class="trash-icon"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+                <div class="cart-product">
+                    <div class="cart-product-img">
+                        <img src="/img/oppo-reno8-pro.jpg">
+                    </div>
+                    <div class="cart-product-name">
+                        Điện thoại Oppo Reno 8 Pro
+                        <br>
+                        <a href="" class="link-detail">Xem chi tiết</a>
+                    </div>
+                    <div class="cart-product-price">
+                        21.000.000đ
+                        <div class="change-count-product">
+                            <span class="change-count">- </span><span id="count-value"> 1 </span><span class="change-count"> +</span>
+                        </div>
+                    </div>
+                    <a href="" class="trash-icon"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+                <div class="cart-product">
+                    <div class="cart-product-img">
+                        <img src="/img/oppo-reno8-pro.jpg">
+                    </div>
+                    <div class="cart-product-name">
+                        Điện thoại Oppo Reno 8 Pro
+                        <br>
+                        <a href="" class="link-detail">Xem chi tiết</a>
+                    </div>
+                    <div class="cart-product-price">
+                        21.000.000đ
+                        <div class="change-count-product">
+                            <span class="change-count">- </span><span id="count-value"> 1 </span><span class="change-count"> +</span>
+                        </div>
+                    </div>
+                    <a href="" class="trash-icon"><i class="fa-solid fa-trash-can"></i></a>
+                </div>
+            </div>
+        </div>
+        <div class="cart-right panel">
+            <div class="cart-header">Xác nhận mua hàng</div>
+            <div class="cart-body">
+                <form action="" method="post">
+                    <div class="cart-line">
+                        <span class="cart-label">Phương thức: </span>
+                        <select class="form-control" name="hinh-thuc-giao-hang">
+                            <option value="1">Nhận tại siêu thị</option>
+                            <option value="2">Nhận hàng tại nhà</option>
+                        </select>
+                    </div>
+                    <div class="cart-line" id="nhan-hang">
+                        <span class="cart-label">Địa chỉ nhận hàng: </span>
+                        <textarea type="text" class="form-control" rows="5" name="dia-chi-nhan-hang"></textarea>
+                    </div>
+                    <div class="cart-line" id="lay-hang">
+                        <span class="cart-label">Chọn siêu thị: </span>
+                        <select class="form-control" name="sieu-thi-nhan-hang">
+                            <option value="1">CQNMobile Nhơn Hòa 1</option>
+                            <option value="2">CQNMobile Nhơn Hòa 2</option>
+                        </select>
+                    </div>
+                    <div class="form-group">
+                        <label for="code">Nhập mã khuyến mãi (Nếu có):</label>
+                        <div class="promo-code">
+                            <div class="input-promo-code">
+                                <input type="text" class="form-control mt-2" name="promo-code">
+                            </div>
+                            <div class="button-promo-code">
+                                <a href="" class="btn btn-danger mt-2"><i class="fa-solid fa-arrow-right"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="cart-note">
+                        <p class="cart-note-header"><u>Lưu ý: </u></p>
+                        <p>- Sản phẩm sẽ có sẵn tại siêu thị ngay lúc bạn đặt hàng thành công</p>
+                        <p>- Sản phẩm sẽ được giao đến nhà của bạn sau khoảng 2-4 ngày</p>
+                        <p>- Bạn có thể kiểm tra sản phẩm trước khi thanh toán</p>
+                    </div>
+                    <div class="cart-confirm">
+                        <div class="cart-total">Tổng tiền: <span class="cart-total-value">63.000.000đ</span></div>
+                        <button class="cart-submit">Đặt hàng</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
